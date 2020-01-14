ca7150623f3926cdeb3c5dca883bdcecbfb11de8
Kotiko29 <kotiko29@yandex.ru>
2020-01-14 14:33:47 +0500

    some fix

--- index.js
+++ index.js
@@ -31,14 +31,4 @@ productButton.onclick = function() {
     }
 };
 
-let productButton = document.querySelector('.product-button'),
-    tablePriceList = document.querySelector('.price-list');
 
-productButton.onclick = function() {
-    console.log('Кнопка нажата!');
-    if (tablePriceList.style.display === `table`) {
-        tablePriceList.style.display = `none`;
-    } else {
-        tablePriceList.style.display = `table`;
-    }
-};
